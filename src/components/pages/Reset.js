62c39f6d9708e06b52fb1dff1f28b9d216c97a07
jonocr <jono.calvo@gmail.com>
2020-03-31 22:24:47 -0700

    Password reset page added

---
+++ src/components/pages/Reset.js
@@ -0,0 +1,115 @@
+import React, { useCallback } from 'react';
+import { withRouter } from "react-router";
+import app from "../base.js";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://github.com/jonocr">
+                Jono's Github
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+const PasswordReset = ({ history }) => {
+    const classes = useStyles();
+
+    const handleReset = useCallback(
+        async event => {
+            event.preventDefault();
+            const { email } = event.target.elements;
+
+            try {
+                await app
+                    .auth()
+                    .sendPasswordResetEmail(email.value)
+                    .then(function() {
+                        // Email sent.
+                      }).catch(function(error) {
+                        // An error happened.
+                      });
+                //history.push("/login");
+            } catch (error) {
+                alert(error);
+            }
+
+        },
+        [history]
+    );
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Reset Password
+        </Typography>
+                <form className={classes.form} noValidate onSubmit={handleReset}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoFocus
+                    />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Send Email
+          </Button>
+                </form>
+            </div>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container>
+    );
+}
+
+export default withRouter(PasswordReset);
